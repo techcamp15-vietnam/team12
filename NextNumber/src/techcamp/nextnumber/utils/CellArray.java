32b92748738afab434ca0d178a6ae48f485b9693
monsterpooka <monster.pooka@gmail.com>
2014-01-09 10:41:05 +0700

    up source code v2
    change way to creat random effect in challenge mode

--- NextNumber/src/techcamp/nextnumber/utils/CellArray.java
+++ NextNumber/src/techcamp/nextnumber/utils/CellArray.java
@@ -5,23 +5,51 @@ package techcamp.nextnumber.utils;
 
 import java.util.Random;
 
-import techcamp.nextnumber.scenes.GameScene;
-
 public class CellArray {
-	public Celldata[] CA = new Celldata[GameScene.LIMIT_SIZE];
+	public Celldata[] CA = new Celldata[25];
 	public int nextint; // The number have touch.
-
+	private int effect[] = {Celldata.DOUBLE,Celldata.BOOM,Celldata.BLIND};
 	// innit the CellArray
 	public CellArray() {
 		for (int i = 0; i < 25; i++)
-			this.CA[i] = new Celldata(0, 0, true);
+			this.CA[i] = new Celldata(0, Celldata.NOMAL, true);
 		nextint = 1;
 	}
 
-	// Create data for a game play. if classic mode mode=1; challenge mode = 4
+	// Create data for a game play. if classic mode mode=1; challenge mode = 2
 	public void CreateData(int mode) {
+		switch (mode) {
+		case 1: CreateNormal();
+		case 2: CreateEffect();
+		}
+	}
+//Create the challenge mode data
+	private void CreateEffect() {
+		CreateNormal();
+		Random r= new Random();
+		int numofeffect= r.nextInt(6)+1;
+		int numoftype[]={0,0,0};
+		for (int i=0;i<2;i++) {
+			Random x= new Random();
+			if (numofeffect==0) break;
+			numoftype[i]=x.nextInt(numofeffect);
+			numofeffect=numofeffect-numoftype[i];
+		}
+		numoftype[2]=numofeffect;
+		for (int i=0;i<3;i++){
+			for (int j=0;j<numoftype[i];j++){
+				Boolean k=true;
+				while (k){
+				Random t = new Random();
+				int id = t.nextInt(25) + 1;
+				if (this.CA[id].geteffect()==0) {this.CA[id].seteffect(effect[i]);k=false;}
+				}
+			}
+		}
+	}
+//Create the normal mode data
+	private void CreateNormal() {
 		boolean[] values = new boolean[26];
-		int effects[] = { Celldata.NOMAL, Celldata.DOUBLE, Celldata.BOOM,Celldata.BLIND };
 		for (int i = 0; i < 26; i++)
 			values[i] = true;
 		for (int i = 0; i < 25; i++) {
@@ -36,10 +64,8 @@ public class CellArray {
 					values[rvalue] = false;
 				}
 			}
-				Random x = new Random();
-				int xvalue = x.nextInt(mode);
-				this.CA[i].seteffect(effects[xvalue]);
 		}
+		
 	}
 
 	// Set the value for a CellData which have ID is i
@@ -84,12 +110,12 @@ public class CellArray {
 				k = i;
 				break;
 			}
-		nextint++;
+		if (k<26) nextint++;
 		return k;
 	}
 
 	// Check complete the game
 	public boolean check_complete() {
-		return (nextint == 26);
+		return (nextint >= 26);
 	}
 }
